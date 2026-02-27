b6eaafebca8805e51aef4802dc4c0fb435b945e7
Wangake <youngwanga254@gmail.com>
2026-02-27 14:50:23 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+__path = process.cwd()
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 50900;
+const { 
+  qrRoute,
+  pairRoute
+} = require('./routes');
+require('events').EventEmitter.defaultMaxListeners = 2000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/qr', qrRoute);
+app.use('/code', pairRoute);
+
+app.get('/pair', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'pair.html'));
+});
+
+app.get('/pair2', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'pair2.html'));
+});
+
+app.get('/pair3', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'pair3.html'));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
+app.get('/health', (req, res) => {
+    res.json({
+        status: 200,
+        success: true,
+        service: 'MEGAN-MD',
+        timestamp: new Date().toISOString()
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`
+Deployment Successful!
+
+ Megan session server Running on http://localhost:` + PORT)
+});
+
+module.exports = app;
